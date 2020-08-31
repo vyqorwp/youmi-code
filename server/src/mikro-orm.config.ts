814991dcf32d3d831b99fc9c197c566982d61755
youmiaddd <youmi.lee@addd.co.kr>
2020-08-31 15:36:15 +0900

    server : typescript 설정 server : db 설정 및 migration

---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,21 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path"
+
+
+export default {
+    migrations: {
+        path: path.join(__dirname,"./migrations"),
+        pattern : /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    dbName : 'postgres',
+    debug: !__prod__ ,
+    type: 'postgresql',
+    password: 'mysecretpassword'
+
+
+
+} as Parameters<typeof MikroORM.init>[0]
+
