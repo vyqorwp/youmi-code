1f835b9c54d94abb60eca2aee81718858a869e15
youmi lee <vyqorwp@gmail.com>
2020-09-02 22:24:54 +0900

    no message

--- server/dist/mikro-orm.config.js
+++ server/dist/mikro-orm.config.js
@@ -6,16 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Post_1 = require("./entities/Post");
 const constants_1 = require("./constants");
 const path_1 = __importDefault(require("path"));
+const User_1 = require("./entities/User");
 exports.default = {
     migrations: {
-        path: path_1.default.join(__dirname, "./migrations"),
+        path: path_1.default.join(__dirname, './migrations'),
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
-    entities: [Post_1.Post],
-    dbName: 'youmiDB',
+    entities: [Post_1.Post, User_1.User],
+    dbName: 'postgres',
     debug: !constants_1.__prod__,
     type: 'postgresql',
-    userName: 'postgres',
-    password: '1234'
+    password: 'mysecretpassword',
 };
 //# sourceMappingURL=mikro-orm.config.js.map
